8143b778872fcf524b44ac7e3cde3722ac07163e
Tristan Matthias <hello@tristanmatthias.com>
2018-06-29 18:02:53 -0700

    Fixed bug with login

--- src/v1/controllers/users/index.ts
+++
@@ -1,9 +0,0 @@
-import {auth, Route} from 'origami-core-lib';
-
-const r = new Route('/api/v1/users');
-module.exports = r;
-
-r.position('pre-store').post(async (req, res, next) => {
-    req.body.password = await auth.hash(req.body.password);
-    next();
-});
