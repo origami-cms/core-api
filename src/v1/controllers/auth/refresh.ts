685d9a91c6590a7fd21de5f94778d1c4f299f3af
Tristan Matthias <hello@tristanmatthias.com>
2018-08-14 16:20:15 +1000

    Authenticated auth refresh

--- src/v1/controllers/auth/refresh.ts
+++ src/v1/controllers/auth/refresh.ts
@@ -1,4 +1,5 @@
 import {auth, Route} from 'origami-core-lib';
+import {auth as authMW} from 'origami-plugin-auth';
 
 const r = new Route('/api/v1/auth/refresh');
 module.exports = r;
@@ -6,18 +7,20 @@ module.exports = r;
 /*
 * Gives a new JWT based on the current one
 */
-r.post(async(req, res, next) => {
+r
+    .use(authMW)
+    .post(async(req, res, next) => {
     // @ts-ignore
-    const secret = req.app.get('secret');
+        const secret = req.app.get('secret');
 
-    const existing = auth.jwtVerify(req.jwt.token, secret);
-    delete existing.iat;
-    delete existing.exp;
+        const existing = auth.jwtVerify(req.jwt.token, secret);
+        delete existing.iat;
+        delete existing.exp;
 
-    const token = auth.jwtSign(existing, secret);
-    const {iat: expires} = auth.jwtVerify(token, secret);
+        const token = auth.jwtSign(existing, secret);
+        const {iat: expires} = auth.jwtVerify(token, secret);
 
-    res.data = {token, expires};
+        res.data = {token, expires};
 
-    await next();
-});
+        await next();
+    });
