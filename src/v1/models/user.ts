80b0aceba58883c2771f2f6b39eca821f443fdfa
Tristan Matthias <hello@tristanmatthias.com>
2018-09-11 14:32:34 +1000

    Made email field required for user

--- src/v1/models/user.ts
+++ src/v1/models/user.ts
@@ -4,7 +4,7 @@ export default {
         id: 'uuid',
         fname: {type: String, required: true},
         lname: {type: String, required: false},
-        email: {type: 'email', required: false, unique: true},
+        email: {type: 'email', required: true, unique: true},
         password: {type: String, required: true, hidden: true}
     }
 };
