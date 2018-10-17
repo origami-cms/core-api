ff286d0489b54b8cca8af98ac14113a0d273832a
Tristan Matthias <hello@tristanmatthias.com>
2018-10-17 11:40:15 +1100

    fix: updated apps to new manifest version

--- src/v1/controllers/apps/index.ts
+++ src/v1/controllers/apps/index.ts
@@ -1,20 +1,19 @@
+import {Route} from 'origami-core-lib';
+import {App} from 'origami-core-server';
 import {auth} from 'origami-plugin-auth';
-import {Route, Origami} from 'origami-core-lib';
 
 const r = new Route('/api/v1/apps');
 module.exports = r;
 
 r
     .position('store')
-    .use(auth)
-    .get(async(req, res, next) => {
-        res.data = Object.entries(res.app.get('apps') as {[name: string]: Origami.AppManifest})
-        .reduce((apps, [name, manifest]) => {
-            apps[name] = {
-                name: manifest.name,
-                icon: manifest.icon
-            };
+    .get('auth', async(req, res, next) => {
+
+        res.data = Object.entries(res.app.get('apps') as {[name: string]: App.App})
+        .reduce((apps, [name, app]) => {
+            apps[name] = app.entry!;
             return apps;
-        }, {} as {[name: string]: {name: string, icon: object}});
+        }, {} as {[name: string]: App.EntryResponse});
+
         next();
     });
